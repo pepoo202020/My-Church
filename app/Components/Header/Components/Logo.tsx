3b9d320a0e3334bb573f8ca73726cbb94283d1d7
Abanob Shenoda <121344723+pepoo202020@users.noreply.github.com>
2024-07-01 15:00:52 +0300

    finished logo

---
+++ app/Components/Header/Components/Logo.tsx
@@ -0,0 +1,39 @@
+import { Arbutus, Lalezar } from "next/font/google";
+import Image from "next/image";
+import React from "react";
+
+const lalezarFont = Lalezar({
+  subsets: ["latin", "arabic"],
+  weight: ["400"],
+  display: "swap",
+});
+
+const arbutus = Arbutus({
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
+});
+
+export const Logo = () => {
+  return (
+    <div className="flex items-center justify-center gap-5">
+      <div>
+        <Image
+          src="/image/logo.png"
+          alt="church logo image"
+          width={134}
+          height={127}
+          className="drop-shadow-lg w-[134px] h-[127px]"
+        />
+      </div>
+      <div className="flex flex-col items-start justify-center gap-0">
+        <div className={`${lalezarFont.className} text-[30px]`}>
+          كنيسة الشهيد ابادير واخته الشهيده ايريني بأسيوط
+        </div>
+        <div className={`${arbutus.className} text-[20px]`}>
+          Martyr Abader And Eriny. Asyut
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,16 +1,28 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { BirthDate } from "./Components/BirthDate";
 import { CopticDate } from "./Components/CopticDate";
+import { Logo } from "./Components/Logo";
+import { Cairo } from "next/font/google";
+
+const cairoFont = Cairo({
+  subsets: ["latin", "arabic"],
+  weight: ["400", "600", "700"],
+  display: "swap",
+});
 
 export const Header = () => {
   return (
     <header className="flex flex-col w-full items-center justify-center sticky top-0 z-50 py-2">
-      <div className=" w-full flex items-center justify-between">
+      <div
+        className={`w-full flex items-center justify-between ${cairoFont.className} text-[20px] font-bold`}
+      >
         <BirthDate />
         <CopticDate />
       </div>
-      <div className=" p-5 -mt-5">Logo</div>
+      <div className=" p-5 -mt-6">
+        <Logo />
+      </div>
       <div>nav</div>
     </header>
   );
