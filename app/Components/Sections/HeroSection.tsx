cafa440eb49600aa3e5bf2c778bd787a1d86288f
Abanob Shenoda <121344723+pepoo202020@users.noreply.github.com>
2024-07-10 12:55:35 +0300

    Main page: hero section

--- app/Components/Sections/HeroSection.tsx
+++ app/Components/Sections/HeroSection.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export const HeroSection = () => {
   return (
     <div className="w-full h-screen relative overflow-y-hidden">
-      <div className="w-full h-full absolute top-0 right-0">
+      <div className="w-full h-full ">
         <Image
           src={"/image/bg.jpg"}
           alt="hero bg"
@@ -19,6 +19,15 @@ export const HeroSection = () => {
           height={869}
           className="bg-cover w-full block lg:hidden"
         />
+      </div>
+      <div className="absolute lg:top-12 top-8 lg:right-[114px] right-0 z-10 text-white lg:w-[870px] w-full px-2 lg:mx-0   flex flex-col items-center lg:gap-[65px] gap-4">
+        <h1 className="almarai lg:text-[99px] text-[53px] lg:font-extrabold font-bold">
+          مرحباً بكم
+        </h1>
+        <h1 className="lg:changa lemonada lg:text-[53px] text-[18px] font-bold text-center ">
+          الموقع <span className="text-[#FAFF00]">الرسمي</span> لكنيسة الشهيد
+          ابادير واخته الشهيده ايريني
+        </h1>
       </div>
     </div>
   );
